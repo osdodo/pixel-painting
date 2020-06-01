5e4480fc0af2aaa12649b1d5c0050e596f79d603
osdodo <2925007984@qq.com>
2020-06-01 10:48:42 +0800

    去掉赞赏码

--- src/pages/personal/personal.js
+++ src/pages/personal/personal.js
@@ -8,32 +8,32 @@ class Personal extends Component {
         navigationBarTitleText: '个人中心'
     }
 
-    componentWillMount() {
-        wx.cloud.init({
-            traceUser: true
-        })
-    }
+    // componentWillMount() {
+    //     wx.cloud.init({
+    //         traceUser: true
+    //     })
+    // }
 
-    onReward() {
-        Taro.previewImage({
-            urls: ['cloud://pixel-painting-bkykm.7069-pixel-painting-bkykm-1301723573/admire-qrcode.jpg'],
-        }).then(() => {
+    // onReward() {
+    //     Taro.previewImage({
+    //         urls: ['cloud://pixel-painting-bkykm.7069-pixel-painting-bkykm-1301723573/admire-qrcode.jpg'],
+    //     }).then(() => {
 
-        }).catch(err => {})
-    }
+    //     }).catch(err => {})
+    // }
 
     render() {
         return (
             <View>
                 <View className='block'>
-                    <View className='item' onClick={this.onReward}>
+                    {/* <View className='item' onClick={this.onReward}>
                         <View className='left'>
                             <Text className='iconfont icon-dashang' style='font-size: 22px;'></Text>
                             <Text className='title'>请作者喝咖啡</Text>
                         </View>
                         <Text className='iconfont icon-jinru' style='font-size: 15px;'></Text>
                     </View>
-                    <View className='line'></View>
+                    <View className='line'></View> */}
                     <Button hoverClass='none' open-type='contact'>
                         <View className='item'>
                             <View className='left'>
